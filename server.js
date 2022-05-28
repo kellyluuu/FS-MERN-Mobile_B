73acc0f2099ed5d52ab41ce2ae90360c62bde2b0
KELLY LUU <kellyluuu@gmail.com>
2022-05-28 12:29:49 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+require ("dotenv").config()
+const { PORT = 3001, DATABASE_URL } = process.env
+const express = require("express")
+const app = express ()
+
+const cors = require("cors")
+const morgan = require ("morgan")
+
+const mongoose = require("mongoose")
+mongoose.connect(DATABASE_URL)
+mongoose.connection
+    .on("open", ()=>console.log("MongoDB connected"))
+    .on("close",()=> console.log("MongoDB disconnected"))
+    .on ("error",()=> console.log(error))
+
+const MenuSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    catagory: String,
+    price: Number,
+})
+const Menu = mongoose.model("Menu", MenuSchema)
+
+app.use(cors())
+app.use (morgan("dev"))
+app.use (express.json())
+
+//main route
+app.get("/", (req,res)=>{
+    res.send ("backend is up")
+})
+
+//INDEX GET ROUTE
+app.get("/Menu", async (req,res)=>{
+    try{res.json(await Menu.find({}))
+}catch(error){
+    res.status(400).json(error)
+    }
+})
+
+//CREATE POST ROUTE 
+app.post("/Menu",async (req,res)=>{
+    try{
+        res.json(await Menu.create(req.body))
+    }catch (error){
+        res.status(400).json(error)
+    }
+})
+
+//UPDATE PUT ROUTE
+app.put("/Menu/:id", async(req,res)=>{
+    try{
+        res.json(await Menu.findByIdAndUpdate(req.params.id, req.body, {new:true}))
+    }catch(error){
+        res.status(400).json(error)
+    }
+})
+
+//DELETE ROUTE 
+app.delete("/Menu/:id", async(req,res)=>{
+    try{
+        res.json(await Menu.findByIdAndDelete(req.params.id))
+    }catch(error){
+        
+    }
+})
+
+
+
+app.listen(PORT, ()=> console.log (`listening on PORT ${PORT}`))
