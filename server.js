e4545c893a9f23c9620f181b159333080f18d054
KELLY LUU <kellyluuu@gmail.com>
2022-05-30 17:57:02 -0500

    add user login Schema

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require ("dotenv").config()
 const { PORT = 3001, DATABASE_URL } = process.env
 const express = require("express")
 const app = express ()
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+const bcrypt = require("bcrypt");
 
 const cors = require("cors")
 const morgan = require ("morgan")
@@ -13,6 +16,7 @@ mongoose.connection
     .on("close",()=> console.log("MongoDB disconnected"))
     .on ("error",()=> console.log(error))
 
+// Menu Schema
 const MenuSchema = new mongoose.Schema({
     name: String,
     image: String,
@@ -20,6 +24,23 @@ const MenuSchema = new mongoose.Schema({
     price: Number,
 })
 const Menu = mongoose.model("Menu", MenuSchema)
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  })  
+const User = mongoose.model("User", userSchema);
+  
+
+// app.use(
+//     session({
+//         secret: SECRET,
+//         store: MongoStore.create({ mongoUrl: DATABASE_URL }),
+//         saveUninitialized: true,
+//         resave: false,
+//     })
+//     );
 
 app.use(cors())
 app.use (morgan("dev"))
@@ -46,6 +67,19 @@ app.post("/Menu",async (req,res)=>{
         res.status(400).json(error)
     }
 })
+
+// sign up - POST: creates the user in db
+app.post("/signup", async (req, res) => {
+    // encrypt password first
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10));
+    // create the new user in db
+    try{
+        res.json( await User.create(req.body))      
+    }catch(error){
+        res.status(400).json(error)
+    }
+  });
+  
 
 //UPDATE PUT ROUTE
 app.put("/Menu/:id", async(req,res)=>{
